e47daaa2eb1d3351c5b51a431fc9eb4b75f378bc
Elena <ebromeo88@gmail.com>
2023-03-04 20:42:21 +0100

    Modify app

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,31 @@
 import express from 'express';
 import createDebug from 'debug';
-import { _dirname } from './config.js';
+// Import { _dirname } from './config.js';
 import morgan from 'morgan';
 import cors from 'cors';
 import { usersRouter } from './routers/users.router.js';
+// Import path from 'path';
 
 const debug = createDebug('W7:app');
 
 export const app = express();
 app.disable('x-powered-by');
-debug(_dirname);
+
+const corsOrigins = {
+  origin: '*',
+};
+
 app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(cors(corsOrigins));
+app.use(express.json());
 
 app.use((_req, _resp, next) => {
   debug('Soy un middleware');
   next();
 });
 
-const corsOrigins = {
-  origin: '*',
-};
-app.use(cors(corsOrigins));
-app.use(express.json());
+// Debug({ __dirname });
+// app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
