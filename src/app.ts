98cccede7e12197ee87d29bb9badf5f8794b0fde
Elena <ebromeo88@gmail.com>
2023-03-04 15:44:35 +0100

    Modify app file

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import createDebug from 'debug';
 import { _dirname } from './config.js';
 import morgan from 'morgan';
 import cors from 'cors';
+import { usersRouter } from './routers/users.router.js';
 
 const debug = createDebug('W7:app');
 
@@ -12,11 +13,18 @@ debug(_dirname);
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
+app.use((_req, _resp, next) => {
+  debug('Soy un middleware');
+  next();
+});
+
 const corsOrigins = {
   origin: '*',
 };
 app.use(cors(corsOrigins));
 app.use(express.json());
+
+app.use('/users', usersRouter);
 
 app.use('/', (_req, resp) => {
   resp.json({
